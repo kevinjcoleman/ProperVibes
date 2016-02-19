87c70a588da30bbbdbcb92816ec47d30663d9698
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2016-02-18 22:09:42 -0800

    Added a footer to the homepage and a widget that's fixed to the bottom.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,7 @@
 class HomepageController < ApplicationController
 	def home
 		@songs = Song.published_recently.limit(4)
-
+		@songs_for_player = Array.new
+		Song.published_recently.limit(10).each{ |s| @songs_for_player << "#{s.soundcloud_url.to_s}" }
 	end
 end
